e702bb07239c45c4ffa9538780a099664c7c435c
Thien Nguyen <j.thien1551@gmail.com>
2020-11-25 13:04:33 +0700

    Added homepage & related images, fix spelling

--- js/learning01.js
+++ js/learning01.js
@@ -1,19 +1,20 @@
 
-var chu=['Alphabet Game',
+// Activities variable
+var act=['Alphabet Game',
          'Math Game',
          'Music',
-         'Bacsic Color Game',
+         'Basic Color Game',
          'Animals Game',
          'Vegetables Game',
          'The days of the week Game',
          'The week of the year Game',
          'Quiz Game',
-         'Orther Game'
+         'Other Game'
          
         ];
 
-
-var hinh=['image/abc00.png',
+// Activities Icon
+var actIcon=['image/abc00.png',
           'image/math00.png',
           'image/music00.png',
           'image/color00.png',
@@ -22,9 +23,8 @@ var hinh=['image/abc00.png',
           'image/tdotw00.png',
           'image/calender00.png',
           'image/quiz00.png',
-          'image/orther00.png',
+          'image/other00.png',
         ]
-
 
 var n=9;
 
@@ -46,10 +46,10 @@ for (var i=0; i <=n ;i++){
     } else { document.write('<div class="col-sm-2" id="tbg" >'); }
 
     
-    document.write(' <div class="image-box" style="margin-bottom: 12%; margin-top: 3%;  border-radius: 20px; "  >');
-    document.write(' <img src="'+hinh[i]+'" width="150px" height="150px" />');
+    document.write('<div class="image-box" style="margin-bottom: 12%; margin-top: 3%;  border-radius: 20px; "  >');
+    document.write('<img src="'+ actIcon[i] +'" width="150px" height="150px" />');
     document.write('</div>');
-    document.write('<div id="chu" >'+chu[i]+'</div> ');
+    document.write('<div id="activity" >'+ act[i] + '</div> ');
     document.write('</div>');
     document.write('</a>');
     if((i==n) ){
