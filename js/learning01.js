ee912676e7d7d00d42e4e8f32351f83cc00019d9
lenhannhan123 <74607205+lenhannhan123@users.noreply.github.com>
2020-11-25 16:04:11 +0700

    update

--- js/learning01.js
+++ js/learning01.js
@@ -1,33 +1,4 @@
 
-// Activities variable
-var act=['Alphabet Game',
-         'Math Game',
-         'Music',
-         'Basic Color Game',
-         'Animals Game',
-         'Vegetables Game',
-         'The days of the week Game',
-         'The week of the year Game',
-         'Quiz Game',
-         'Other Game'
-         
-        ];
-
-// Activities Icon
-var actIcon=['image/abc00.png',
-          'image/math00.png',
-          'image/music00.png',
-          'image/color00.png',
-          'image/animal00.png',
-          'image/Vegetables00.png',
-          'image/tdotw00.png',
-          'image/calender00.png',
-          'image/quiz00.png',
-          'image/other00.png',
-        ]
-
-var n=9;
-
 
 
 for (var i=0; i <=n ;i++){
