a359a3e7691dde0a588218cb0ba9533cf047ac29
lenhannhan123 <74607205+lenhannhan123@users.noreply.github.com>
2020-12-01 02:30:29 +0700

    Update Register and Log in

--- js/checkerror.js
+++ js/checkerror.js
@@ -64,3 +64,47 @@ $(document).ready(function(){
 
   });
 });
+
+
+
+$(document).ready(function(){
+    $('#loginform').bootstrapValidator({
+        container: '#messages',
+        excluded: ':disabled',
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+        fields: {
+
+          user1:{
+                validators:{
+                    notEmpty: {
+                        message1: 'User Namw is required and cannot be empty'
+                    }
+                }
+            
+            },
+            password1:{
+                validators: {
+                    notEmpty: {
+                        message1: 'Password is required and cannot be empty'
+                    },
+                    stringLength: {
+                        min: 5,
+                        message1: 'Password has at least 5 characters'
+                    },
+                    regexp: {
+                        regexp: /^(\w+[0-9]+|([0-9]+\w+))$/i,
+                        message1: 'Password must contain both alphabet characters and numbers'
+                    }
+                }
+
+            },
+
+        }
+
+
+  });
+});
